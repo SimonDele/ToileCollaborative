23da559a0f9d83f266352df9b60a40bef1699d99
Louis-Bagot <louis.bagot@insa-rouen.fr>
2017-10-20 11:22:16 +0200

    debut modele

---
+++ src/Modele/Group.java
@@ -0,0 +1,10 @@
+package Modele;
+
+import java.util.ArrayList;
+
+public class Group {
+	private ArrayList<Member> memberList;
+	private ArrayList<Boolean> adminList;
+	private Canvas canvas;
+
+}
---
+++ src/Modele/Member.java
@@ -0,0 +1,11 @@
+package Modele;
+
+import java.util.ArrayList;
+
+public class Member {
+	private String pseudo;
+	private String password;
+	private Toolbox toolbox;
+	private ArrayList<Group> groupList;
+	
+}
