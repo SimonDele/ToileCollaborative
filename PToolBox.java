267407c29e517f7db51e19ab275677c6381050a7
Lois <quentin.loiseau@wanadoo.fr>
2017-12-15 10:32:02 +0100

    Size
    add ListenerSize & modif PToolBox

---
+++ PToolBox.java
@@ -0,0 +1,45 @@
+package View;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import Controller.ListenerSize;
+import Controller.ListenersShapes;
+import Modele.Toolbox;
+
+public class PToolBox extends JPanel {
+
+	JButton circle;
+	JButton sizeSmall,sizeNormal,sizeLarge;
+
+	ListenersShapes listenersToolBox;
+	ListenerSize listenerSize;
+
+	public PToolBox(Toolbox toolbox) {
+		circle = new JButton(new ImageIcon(this.getClass().getResource("ressources/circle.png")));
+		sizeSmall = new JButton("Small");
+		sizeNormal = new JButton("Normal");
+		sizeLarge = new JButton("Large");
+
+		listenersToolBox = new ListenersShapes(toolbox, circle);
+		circle.addActionListener(listenersToolBox);
+
+		listenerSize = new ListenerSize(toolbox,sizeSmall);
+		sizeSmall.addActionListener(listenerSize);
+		sizeNormal.addActionListener(listenerSize);
+		sizeLarge.addActionListener(listenerSize);
+
+
+
+		this.add(circle);
+		this.add(sizeSmall);
+		this.add(sizeNormal);
+		this.add(sizeLarge);
+
+
+		this.setBackground(Color.black);
+	}
+}
